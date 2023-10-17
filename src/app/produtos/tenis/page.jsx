cc1b583cb463162a4672d0a2f99c8bd7b838f2ef
rm552008 <rm552008@fiap.com.br>
2023-10-17 09:54:10 -0300

    utilizando parâmetros slug e camisa

---
+++ src/app/produtos/tenis/page.jsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Tenis() {
+    return(
+        <div>
+            <h1>Tênis</h1>
+            <div>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut nostrum dolore rem debitis nesciunt blanditiis, veniam quae beatae ducimus cum vitae ad nobis a modi accusantium dolor odit? Accusamus commodi neque, similique quisquam nostrum veritatis libero, corporis ducimus maiores molestias facilis tempore quod blanditiis minus, quos odit iusto ipsum fuga sed nesciunt id aut aperiam repellendus dolores! Mollitia, voluptatibus quae. Voluptatibus molestias optio id voluptatem, eaque officia et cupiditate, quibusdam hic nulla dolor excepturi nam non ipsam illum aliquam harum pariatur praesentium saepe quaerat, doloribus qui rem. Neque ratione consequuntur numquam sapiente quos, labore quis! Est autem tempore odio earum?
+                <p><Link href="/">Voltar...</Link></p>
+           </div>
+        </div>
+    )
+}
