833144528e85c8b1a13712b138f38c4635941f2a
douglas <douglas.arau@hotmail.com>
2023-10-19 10:25:40 -0300

    utilizando css tailwind

---
+++ src/components/Rodape/Rodape.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Rodape() {
+  return (
+    <footer className="bg-gray-800 text-white h-16 fixed w-full bottom-0">
+       <div>
+            <p>Desenvolvido por: <span>rm552008</span></p>
+            <p>NEXT.js - React.js - GitHub - TailWindCss</p>
+       </div>
+    </footer>
+  )
+}
